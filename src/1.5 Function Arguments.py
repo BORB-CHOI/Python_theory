e9a52bcbf2bc9c5cb1904ff314eec14c15c6923a
Your Name <you@example.com>
2020-05-10 23:42:27 +0900

    Function

---
+++ src/1.5 Function Arguments.py
@@ -0,0 +1,17 @@
+def plus(a, b=0):
+    print(a+b)
+
+
+def minus(a, b=0):
+    print(a-b)
+
+
+def say_hello(name="anonymous"):
+    print("hello", name)
+
+
+plus(2, 5)
+minus(2)
+
+say_hello()
+say_hello("borb")
