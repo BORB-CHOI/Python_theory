4d5786150b9ec23ded27063d7e9eec8834a723c0
Your Name <you@example.com>
2020-05-10 23:57:26 +0900

    Return

---
+++ src/1.6 Returns.py
@@ -0,0 +1,13 @@
+def p_plus(a, b):
+    print(a+b)
+
+
+def r_plus(a, b):
+    return a + b
+    print("asdfasdfasdfasdf")
+
+
+p_result = p_plus(2, 3)
+r_result = r_plus(2, 3)
+
+print(p_result, r_result)
